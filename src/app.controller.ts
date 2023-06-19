ccc09d838f91651a470f3c46ca241e8b1c2159b9
jayashibus <jayashibu@gmail.com>
2023-06-19 22:51:26 +0530

    PUT and DELETE implemented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ import {
   Post,
   Put,
   Body,
+  HttpCode,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ReportType, data } from 'src/data';
@@ -50,12 +51,36 @@ export class AppController {
   }
 
   @Put(':id')
-  updateReport() {
-    return 'Updated';
+  updateReport(
+    @Param('type') type: String,
+    @Param('id') id: String,
+    @Body() body: { amount: number; source: string },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const reportToUpdate = data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+
+    if (!reportToUpdate) return;
+
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+
+    data.report[reportIndex] = { ...data.report[reportIndex], ...body };
+    return data.report[reportIndex];
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport() {
+  deleteReport(@Param('id') id: String) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if (reportIndex === -1) return;
+
+    data.report.splice(reportIndex, 1);
+
     return 'Deleted';
   }
 }
